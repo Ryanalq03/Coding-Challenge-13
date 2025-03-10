25c16d36b7f079fbb10fa33aa051ee986c886940
Ryan Alqasemi <alqasemir@usf.edu>
2025-03-09 22:41:00 -0400

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,3 +1,28 @@
 //Coding Challenge #13
 
-//Task 1: Base Structure
+//Task 2: Employee Cards
+
+function createEmployeeCard(name, position) {
+
+    // This creates the employee card elements
+    const employeeCard = document.createElement('div');
+    employeeCard.setAttribute('class','employee-card');  
+    employeeCard.setAttribute('id', 'employeeCard');  
+    
+    //creates the employee name element
+    const employeeName = document.createElement('h2');
+    employeeName.textContent = name; 
+    employeeCard.appendChild(employeeName);
+    
+    //Element with employee position added
+    const employeePosition = document.createElement('p');
+    employeePosition.textContent = position; 
+    employeeCard.appendChild(employeePosition);
+    
+    // This is a button that removes the card
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.setAttribute('class', 'remove-button');
+    Clipboard.appendChild(removeButton);
+}
+
